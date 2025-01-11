1b17eedf4251d2c1bc684c5889ab071dff29bd7a
Jan <janbscholl@gmail.com>
2025-01-11 11:58:12 +0100

    improved API call code

--- scripts/call_API.py
+++ scripts/call_API.py
@@ -1,13 +1,16 @@
 import requests
 from src.strom import utils
+from datetime import date
 
-
+query = "https://web-api.tp.entsoe.eu/api?documentType=A44"
+timeframe = "&periodStart=202501110100&periodEnd=202501120100"
+domain = "&out_Domain=10YAT-APG------L&in_Domain=10YAT-APG------L"
+security = "&securityToken="
 apikey = utils.get_api_key('../config/apiKey.txt') #please see readme to see how to create your config folder with the API key
-query = "https://web-api.tp.entsoe.eu/api?documentType=A44&periodStart=202501102200&periodEnd=202501112200&out_Domain=10YAT-APG------L&in_Domain=10YAT-APG------L&securityToken="
 
-response = requests.get(query + apikey)
+response = requests.get(query + timeframe + domain + security + apikey)
 
 print(response.text)
 
-with open("../data/output_file.txt", "wb") as file:  # Use "wb" to write in binary mode
+with open("../data/" + str(date.today()) + "-API-response.json", "wb") as file:  # Use "wb" to write in binary mode
     file.write(response.content)
