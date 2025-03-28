bf69b49c1eeaaae4aadebd806416e974a6ca2f14
dviladrich95 <daniel.viladrich.h@gmail.com>
2025-03-28 03:50:43 +0100

    renamed temperature to outdoor temperature

--- strom/optimization_utils.py
+++ strom/optimization_utils.py
@@ -28,7 +28,7 @@ class House:
     def __init__(self, C_air=0.56, C_walls=3.5, R_internal=1.0,
                 R_external=6.06, Q_heater=2.0, min_temperature=18.0, 
                 max_temperature=24.0, init_indoor_temp = 18.5,
-                init_wall_temp = 20.0, freq='h'):
+                init_wall_temp = 18.5, freq='h'):
         
         self.C_air = C_air
         self.C_walls = C_walls
@@ -55,7 +55,7 @@ def find_heating_decision(temp_price_df, house, heating_mode):
         dt = 1.0
 
     time_steps = len(state_df)
-    outdoor_temperature = state_df["Temperature"]
+    outdoor_temperature = state_df["Outdoor Temperature"]
     
 
     heater_state = cp.Variable(time_steps)
@@ -89,7 +89,7 @@ def find_heating_decision(temp_price_df, house, heating_mode):
     if heating_mode == "optimal":
         obj = cp.sum(cp.multiply(state_df["Price"], heater_state * house.Q_heater)) + 0.1*cp.norm(cp.diff(indoor_temperature), 1)
     elif heating_mode == "baseline":
-        obj = cp.sum(cp.maximum(house.min_temperature-indoor_temperature, 0))
+        obj = cp.sum(cp.square(house.min_temperature-indoor_temperature))
     objective = cp.Minimize(obj)
     
     # Solve optimization
@@ -148,7 +148,7 @@ def get_state_df(temp_price_df, decision, house):
 
     for t in range(time_steps - 1):
         heat_loss_air = (indoor_temperature[t] - wall_temperature[t]) / house.R_internal
-        heat_loss_wall = (wall_temperature[t] - state_df['Temperature'][t]) / house.R_external
+        heat_loss_wall = (wall_temperature[t] - state_df['Outdoor Temperature'][t]) / house.R_external
         
         indoor_temperature[t + 1] = indoor_temperature[t] + dt * (house.Q_heater * state_df['Decision'][t] - heat_loss_air) / house.C_air
         wall_temperature[t + 1] = wall_temperature[t] + dt * (heat_loss_air - heat_loss_wall) / house.C_walls
@@ -202,56 +202,81 @@ def plot_state(state_df, case_label, plot_price=True):
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
     return fig 
 
-def plot_combined_cases(state_opt_df,state_base_df):
+def plot_combined_cases(state_opt_df, state_base_df, plot_heater_state=True, plot_price=True, plot_outdoor_temp=True):
     #save the plots in the plots folder
     os.chdir(api_utils.find_root_dir())
+    legends = []
+    fig, ax1 = plt.subplots(figsize=(14, 7), constrained_layout=True)
 
-    fig, ax1 = plt.subplots()
-
+    # Always plot cost on the first axis
     color = 'tab:blue'
     ax1.set_xlabel('Time (h)')
     ax1.set_ylabel('Cost (€)', color=color)
     ax1.plot(state_opt_df['Cost'].cumsum(), color=color, linestyle='-')
     ax1.plot(state_base_df['Cost'].cumsum(), color=color, linestyle='--')
     ax1.tick_params(axis='y', labelcolor=color)
-    ax1.tick_params(axis='x', rotation=45)  # Rotate x-axis tick labels
+    ax1.tick_params(axis='x', rotation=45)
+    
+    # Keep track of the number of additional axes
+    additional_axes_count = 0
+    
+    # Indoor Temperature Axis
+    if plot_outdoor_temp:
+        ax2 = ax1.twinx()  
+        additional_axes_count += 1
+        ax2.spines['right'].set_position(('outward', 60 * additional_axes_count))
+        color = 'tab:red'
+        ax2.set_ylabel('Indoor Temperature (°C)', color=color)
+        ax2.plot(state_opt_df['Indoor Temperature'], color=color, linestyle='-')
+        ax2.plot(state_base_df['Indoor Temperature'], color=color, linestyle='--')
+        ax2.tick_params(axis='y', labelcolor=color)
 
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    # Heater State Axis
+    if plot_heater_state:
+        ax3 = ax1.twinx()
+        additional_axes_count += 1
+        ax3.spines['right'].set_position(('outward', 60 * additional_axes_count))
+        color = 'tab:green'
+        ax3.plot(state_opt_df['Decision'], color=color, linestyle='-')
+        ax3.plot(state_base_df['Decision'], color=color, linestyle='--')
+        ax3.set_ylabel('Heater State', color=color)
+        ax3.tick_params(axis='y', labelcolor=color)
 
-    color = 'tab:red'
-    ax2.set_ylabel('Indoor Temperature (°C)', color=color)  # we already handled the x-label with ax1
-    ax2.plot(state_opt_df['Indoor Temperature'], color=color, linestyle='-')
-    ax2.plot(state_base_df['Indoor Temperature'], color=color, linestyle='--')
-    ax2.tick_params(axis='y', labelcolor=color)
-    ax2.tick_params(axis='x', rotation=45)  # Rotate x-axis tick labels
+    # Price Axis
+    if plot_price:
+        ax4 = ax1.twinx()
+        additional_axes_count += 1
+        ax4.spines['right'].set_position(('outward', 60 * additional_axes_count))
+        color = 'tab:grey'
+        ax4.plot(state_opt_df['Price'], color=color)
+        ax4.set_ylabel('Price (€/kWh)', color=color)
+        ax4.tick_params(axis='y', labelcolor=color)
 
+    # Legend setup with conditional plotting
+    legends = []
+    if True:  # Always add cost legend
+        legends.append((['Optimal Cost', 'Baseline Cost'], ax1, 'tab:blue'))
+    
+    if plot_outdoor_temp:
+        legends.append((['Optimal Indoor Temperature', 'Baseline Indoor Temperature'], ax2, 'tab:red'))
+    
+    if plot_heater_state:
+        legends.append((['Optimal Heater State', 'Baseline Heater State'], ax3, 'tab:green'))
+    
+    if plot_price:
+        legends.append((['Price'], ax4, 'tab:grey'))
 
-    ax3 = ax1.twinx()
-    color = 'tab:green'
-    ax3.spines['right'].set_position(('outward', 60))
-    ax3.plot(state_opt_df['Decision'], color=color, linestyle='-')
-    ax3.plot(state_base_df['Decision'], color=color, linestyle='--')
-    ax3.set_ylabel('Heater State', color=color)
-    ax3.tick_params(axis='y', labelcolor=color)
+    # Place legends
+    for i, (legend_text, ax, color) in enumerate(legends):
+        ax.legend(
+            legend_text, 
+            loc='lower left', 
+            bbox_to_anchor=(0.30*i, 1.01), 
+            ncol=len(legend_text),
+            prop={'size': 8}
+        )
 
-    # add a third line in yellow with the electricity price
-    ax4 = ax1.twinx()
-    # make the color a pale blue
-    color = 'tab:grey'
-
-    ax4.spines['right'].set_position(('outward', 120))
-    ax4.plot(state_opt_df['Price'], color=color)
-    ax4.set_ylabel('Price (€/kWh)', color=color)
-    ax4.tick_params(axis='y', labelcolor=color)
-
-    # Add legends
-    # Add legends outside the plot area
-    ax1.legend(['Optimal Cost', 'Baseline Cost'], loc='upper left', bbox_to_anchor=(-0.2, 1.2))
-    ax2.legend(['Optimal Temperature', 'Baseline Temperature'], loc='upper left', bbox_to_anchor=(0.35, 1.2))
-    ax3.legend(['Optimal Heater State', 'Baseline Heater State'], loc='upper left', bbox_to_anchor=(0.5, 1.2))
-    ax4.legend(['Price'], loc='upper left', bbox_to_anchor=(0.75, 1.2))
-
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.subplots_adjust(top=0.85)  # Make room for legends
 
     return fig
 
