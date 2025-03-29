21cd5a653674bfe94ec1aa59a9a4447af955734f
dviladrich95 <daniel.viladrich.h@gmail.com>
2025-03-29 07:43:38 +0100

    separated plot_utils

--- strom/__init__.py
+++ strom/__init__.py
@@ -1,6 +1,7 @@
 from .api_utils import find_root_dir, get_weather_data, get_price_series
-from .optimization_utils import find_heating_output, compare_output_costs, plot_state
+from .optimization_utils import find_heating_output, compare_output_costs
 from .data_utils import get_temp_price_df, join_data
+from .plot_utils import plot_state
 
 __all__ = [
     'find_root_dir',
