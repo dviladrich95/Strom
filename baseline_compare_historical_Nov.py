a1c78418144264c53ac6437a78c4a789b73f57b5
dviladrich95 <daniel.viladrich.h@gmail.com>
2025-04-01 20:42:50 +0200

    rearrange

--- baseline_compare_historical_Nov.py
+++ baseline_compare_historical_Nov.py
@@ -7,7 +7,7 @@ from strom.plot_utils import plot_combined_cases
 import pandas as pd
 import matplotlib.pyplot as plt
 
-temp_price_df = pd.read_csv('data/Temp_Price_Barcelona_Nov.csv', index_col="Timestamp", parse_dates=["Timestamp"])
+
 
 house = House(
     C_air = 0.56,
@@ -24,6 +24,7 @@ house = House(
     freq = '15min'
     )
 
+temp_price_df = pd.read_csv('data/Temp_Price_Barcelona_Nov.csv', index_col="Timestamp", parse_dates=["Timestamp"])
 optimal_state_df, baseline_state_df = compare_output_costs(temp_price_df,house)
 
 fig = plot_combined_cases(optimal_state_df, baseline_state_df)
